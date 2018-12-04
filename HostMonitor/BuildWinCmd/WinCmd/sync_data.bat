5a2b8f5d2b5276ac70ec91e1cdd89e903356c122
idea4good <idea4good@outlook.com>
2018-12-04 16:57:56 +0800

    fix Chinese date format issue

--- HostMonitor/BuildWinCmd/WinCmd/sync_data.bat
+++ HostMonitor/BuildWinCmd/WinCmd/sync_data.bat
@@ -7,11 +7,14 @@ if NOT "7" == "%argC%" (
     )
 
 set url=https://api.powerbi.com/beta/72f988bf-86f1-41af-91ab-2d7cd011db47/datasets/e0e71bab-d932-4bb8-bfcf-faec5aeadb60/rows?key=J9cQJK6rZyLYQ9NsuWV6RYyrgODk1Wu29tuzwMyBET62Xp1dxuJ5iR%%2B0ZtL6o0ams029nNUhWci%%2B%%2B4GzxXHyBQ%%3D%%3D
-set date=
-for /f "tokens=2-4 delims=/ "  %%a in ("%date%") do (set MM=%%a& set DD=%%b& set YYYY=%%c)
-set datetime=%YYYY%-%MM%-%DD%T%time: =0%0+0800
-set devie_info=%7-%USERNAME%
 
+for /f "tokens=2-4 delims=/ "  %%a in ("%date%") do (set MM=%%a& set DD=%%b& set YYYY=%%c)
+set YY=%YYYY:~0,2%
+if not "%YY%" == "20" (rem For Chinese date format
+    for /f "tokens=1-3 delims=/ "  %%a in ("%date%") do (set YYYY=%%a& set MM=%%b& set DD=%%c))
+set datetime=%YYYY%-%MM%-%DD%T%time: =0%0+0800
+
+set devie_info=%7-%USERNAME%
 set raw_data=[{^
 \"hr\" : %1,^
 \"spo2\" :%2,^

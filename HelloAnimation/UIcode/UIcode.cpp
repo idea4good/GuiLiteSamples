eb73129aa2cba6d98099b5a5463d59f43c26a275
idea4good <idea4good@outlook.com>
2018-12-19 22:30:26 +0800

    HelloAnimation -- basic function well

--- HelloAnimation/UIcode/UIcode.cpp
+++ HelloAnimation/UIcode/UIcode.cpp
@@ -15,34 +15,95 @@
 #include <stdlib.h>
 #include <string.h>
 
-const int UI_WIDTH = 512;
-const int UI_HEIGHT = 768;
-
+const int UI_WIDTH = 238;
+const int UI_HEIGHT = 169;
 //////////////////////// define widgets & map message ////////////////////////
 enum WND_ID
 {
-	ID_ROOT = 1
+	ID_ROOT = 1,
+	ID_DESKTOP,
+	ID_BUTTON
+};
+class c_root : public c_wnd
+{
+	virtual c_wnd* clone() { return new c_root(); }
 };
 
+class c_myUI : public c_wnd
+{
+	virtual c_wnd* clone() { return new c_myUI(); }
+	virtual void on_paint(void);
+	void on_clicked(unsigned int ctrl_id);
+	GL_DECLARE_MESSAGE_MAP()//delcare message
+};
+
+//map message
+GL_BEGIN_MESSAGE_MAP(c_myUI)
+ON_GL_BN_CLICKED(ID_BUTTON, c_myUI::on_clicked)
+GL_END_MESSAGE_MAP()
+
+extern const BITMAP_INFO frame_00_bmp, frame_01_bmp, frame_02_bmp, frame_03_bmp, frame_04_bmp, frame_05_bmp, frame_06_bmp;
+void c_myUI::on_clicked(unsigned int ctrl_id)
+{
+	c_rect rect;
+	get_screen_rect(rect);
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_00_bmp);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_01_bmp);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_02_bmp);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_03_bmp);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_04_bmp);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_05_bmp);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_06_bmp);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+}
+
+void c_myUI::on_paint()
+{
+	c_rect rect;
+	get_screen_rect(rect);
+	c_bitmap::draw_bitmap(m_surface, m_z_order, c_my_resource::get_bmp(BITMAP_CUSTOM1), rect.m_left, rect.m_top);
+}
+
 //////////////////////// layout UI ////////////////////////
-static c_slide_group s_root;
+static c_root s_root;
+static c_myUI s_myUI;
+static c_button	s_button;
+
+static WND_TREE s_myUI_children[] =
+{
+	{(c_wnd*)&s_button, ID_BUTTON, (char*)NULL, 0, 0, 150, 40, NULL},
+	{ NULL,0,0,0,0,0,0 }
+};
+
 static WND_TREE s_root_children[] =
 {
+	//start button
+	{(c_wnd*)&s_myUI, ID_DESKTOP, 0, 0, 0, UI_WIDTH, UI_HEIGHT, s_myUI_children},
 	{ NULL,0,0,0,0,0,0 }
 };
 
 //////////////////////// start UI ////////////////////////
-//extern const BITMAP_INFO ten_bmp, jack_bmp, queen_bmp, king_bmp, ace_bmp;
+//extern const FONT_INFO KaiTi_33B;
 static c_fifo s_hid_fifo;
 static c_display* s_display;
 void load_resource()
 {
+	c_my_resource::add_bitmap(BITMAP_CUSTOM1, &frame_00_bmp);
+	//c_my_resource::add_font(FONT_DEFAULT, &KaiTi_33B);//for button
+	c_my_resource::add_color(WND_FORECOLOR, GL_RGB(36, 36, 36));//for button
+	c_my_resource::add_color(WND_BACKCOLOR, GL_RGB(255, 255, 255));//for button
 }
 
 void create_ui(void* phy_fb, int screen_width, int screen_height, int color_bytes)
 {
 	load_resource();
-	s_display = new c_display(phy_fb, screen_width, screen_height, UI_WIDTH, UI_HEIGHT, color_bytes, (1 + 5)/*1 root + 5 pages*/);
+	s_display = new c_display(phy_fb, screen_width, screen_height, UI_WIDTH, UI_HEIGHT, color_bytes, 1);
 	c_surface* surface = s_display->alloc_surface(&s_root, Z_ORDER_LEVEL_1);
 	surface->set_active(true);
 
@@ -50,31 +111,31 @@ void create_ui(void* phy_fb, int screen_width, int screen_height, int color_byte
 	s_root.connect(NULL, ID_ROOT, 0, 0, 0, UI_WIDTH, UI_HEIGHT, s_root_children);
 	s_root.show_window();
 
-	new c_gesture(&s_root, &s_root, &s_hid_fifo);
-	while(1)
+	new c_gesture(&s_root, NULL, &s_hid_fifo);
+	while (1)
 	{
 		thread_sleep(1000000);
 	}
 }
 
 //////////////////////// interface for all platform ////////////////////////
-void startHelloSlide(void* phy_fb, int width, int height, int color_bytes)
+void startHelloAnimation(void* phy_fb, int width, int height, int color_bytes)
 {
 	create_ui(phy_fb, width, height, color_bytes);
 }
 
-int sendTouch2HelloSlide(void* buf, int len)
+int sendTouch2HelloAnimation(void* buf, int len)
 {
 	ASSERT(len == sizeof(MSG_INFO));
 	return s_hid_fifo.write(buf, len);
 }
 
-void* getUiOfHelloSlide(int* width, int* height, bool force_update)
+void* getUiOfHelloAnimation(int* width, int* height, bool force_update)
 {
 	return s_display->get_updated_fb(width, height, force_update);
 }
 
-int captureUiOfHelloSlide()
+int captureUiOfHelloAnimation()
 {
 	return s_display->snap_shot("snap_short.bmp");
 }

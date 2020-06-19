9123046a2a4c6970793d34697f546b7de1f604d1
idea4good <idea4good@outlook.com>
2020-06-19 23:09:09 +0800

    add helloMario/buildWin32

--- HelloMario/UIcode/UIcode.cpp
+++ HelloMario/UIcode/UIcode.cpp
@@ -18,6 +18,7 @@ extern const BITMAP_INFO title_bmp, background_bmp, step1_bmp, step2_bmp, step3_
 
 static BITMAP_INFO s_frames[] = { frame_00_bmp, frame_01_bmp, frame_02_bmp,  frame_03_bmp,  frame_04_bmp,  frame_05_bmp, frame_06_bmp,  frame_07_bmp,  frame_08_bmp,  frame_09_bmp,  frame_10_bmp,  frame_11_bmp, frame_12_bmp, frame_13_bmp };
 static c_surface* s_surface;
+static c_display* s_display;
 
 void do_shoryuken()
 {
@@ -108,12 +109,14 @@ void create_ui(void* phy_fb, int screen_width, int screen_height, int color_byte
 		static c_surface surface(UI_WIDTH, UI_HEIGHT, color_bytes, Z_ORDER_LEVEL_1);
 		static c_display display(phy_fb, screen_width, screen_height, &surface);
 		s_surface = &surface;
+		s_display = &display;
 	}
 	else
 	{//for MCU without framebuffer
 		static c_surface_no_fb surface_no_fb(UI_WIDTH, UI_HEIGHT, color_bytes, gfx_op, Z_ORDER_LEVEL_1, c_rect(0, UI_HEIGHT - background_bmp.height, UI_WIDTH - 1, UI_HEIGHT - 1));
-		static c_display display_top(phy_fb, screen_width, screen_height, &surface_no_fb);
+		static c_display display(phy_fb, screen_width, screen_height, &surface_no_fb);
 		s_surface = &surface_no_fb;
+		s_display = &display;
 	}
 
 	s_surface->fill_rect(0, 0, UI_WIDTH - 1, UI_HEIGHT - 1, GL_RGB(131, 110, 83), Z_ORDER_LEVEL_0);
@@ -131,6 +134,11 @@ void create_ui(void* phy_fb, int screen_width, int screen_height, int color_byte
 //////////////////////// interface for all platform ////////////////////////
 extern "C" void startHelloMario(void* phy_fb, int width, int height, int color_bytes, struct EXTERNAL_GFX_OP* gfx_op) {
 	create_ui(phy_fb, width, height, color_bytes, gfx_op);
+}
+
+extern "C" void* getUiOfHelloMario(int* width, int* height, bool force_update)
+{
+	return s_display->get_updated_fb(width, height, force_update);
 }
 
 void sendTouch2HelloMario(int x, int y, bool is_down)

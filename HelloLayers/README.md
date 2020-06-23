7f338102f668b1d9a3933787a2840252322305aa
idea4good <idea4good@outlook.com>
2020-06-24 00:28:53 +0800

    rename hello2Layers to helloLayers

---
+++ HelloLayers/README.md
@@ -0,0 +1,53 @@
+# How to build for Win MFC?
+1. Open "HelloLayers\BuildMFC\HelloLayers.sln" by Visual studio 2017
+2. Click `F5` to build/run `HelloLayers`
+
+# How to build for STM32F103 + ILI934x?
+## Prerequisite:
+- Install Keil uvsion 5.6 or above.
+- Your hardware should be compatible with: Discovery kit/[正点原子开发板](https://item.taobao.com/item.htm?spm=a230r.1.14.20.17b441b9u49Ujg&id=582084489839&ns=1&abbucket=7#detail)/野火霸道开发板.
+## Build
+1. Open "HelloLayers\BuildSTM32F103-Keil\USER\HelloLayers.uvprojx" with Keil uvsion.
+2. Choose your Device type(Default: STM32F103ZE) from option for target.
+3. Build HelloLayers.
+4. Connect your hardware with your host PC, and flash/run HEX file on it.
+
+# How to port on any type of MCU?
+## Prerequisite:
+- You have Keil/IAR project which can enable LCD
+- Your project should implement function `void delay_ms(unsigned short nms)`, which would be used by core/src/adapter/api_none.cpp::thread_sleep
+
+## Porting
+![HowToPorting](../doc/HowToPorting.png)
+1. Open your Keil/IAR project
+2. Import all cpp files from `UIcode` in your project
+3. Encapsulate draw piexel driver in `main.c`
+4. Link your LCD driver with GuiLite, and call UI entry function(e.g, startHelloLayers)
+5. Build & Run
+
+# How to build for Linux?
+## Compile & Run locally:
+1. Compile:
+    - `cd HelloLayers`
+    - `cmake . && make`
+    - `cd BuildLinux`
+    - `chmod 777 *`
+2. Run locally(e.g, Ubuntu):
+    - Run with framebuffer: `sudo ./HelloLayers /dev/fb0`&nbsp;&nbsp;&nbsp;&nbsp;/dev/fb0: The path of framebuffer device file.
+    - Run inside X Window:
+        1. `sudo su`
+        2. `sudo ipcrm -M 1`
+        3. `sudo ./xWindow 240 320 | ./HelloLayers shared-fb`
+
+## Cross compiler & Run on target:
+1. install compiler:
+    - For ARM32: `sudo apt-get install g++-arm-linux-gnueabi gcc-arm-linux-gnueabi`
+    - For ARM64: `sudo apt-get install g++-aarch64-linux-gnu gcc-aarch64-linux-gnu`
+2. Cross compile:
+    - `cd HelloLayers`
+    - For ARM32: `cmake -D CMAKE_C_COMPILER="/usr/bin/arm-linux-gnueabi-gcc" -D CMAKE_CXX_COMPILER="/usr/bin/arm-linux-gnueabi-g++" . && make`
+    - For ARM64: `cmake -D CMAKE_C_COMPILER="/usr/bin/aarch64-linux-gnu-gcc" -D CMAKE_CXX_COMPILER="/usr/bin/aarch64-linux-gnu-g++" . && make`
+3. Run on target Linux device:
+    - Copy BuildLinux/HelloLayers to target Linux device
+    - `chmod 777 HelloLayers`
+    - `sudo ./HelloLayers /dev/fb0`&nbsp;&nbsp;&nbsp;&nbsp;/dev/fb0: The path of framebuffer
---
+++ HelloLayers/UIcode/UIcode.cpp
@@ -0,0 +1,82 @@
+﻿#define GUILITE_ON  //Do not define this macro once more!!!
+#include "GuiLite.h"
+#include <stdlib.h>
+
+#define UI_WIDTH 240
+#define UI_HEIGHT 320
+
+#define LAYER_1_X 70
+#define LAYER_1_Y 110
+#define LAYER_1_WIDTH  100
+#define LAYER_1_HEIGHT 100
+
+static c_surface* s_surface;
+static c_display* s_display;
+//////////////////////// start UI ////////////////////////
+
+void draw_on_layer_0()
+{
+	s_surface->fill_rect(0, 0, UI_WIDTH - 1, UI_HEIGHT - 1, 0, Z_ORDER_LEVEL_0);
+}
+
+void draw_on_layer_1()
+{
+	s_surface->fill_rect(LAYER_1_X, LAYER_1_Y, LAYER_1_X + LAYER_1_WIDTH - 1, LAYER_1_Y + LAYER_1_HEIGHT - 1, GL_RGB(255, 0, 0), Z_ORDER_LEVEL_1);
+}
+
+void clear_layer_1()
+{
+	//c_rect overlapped_rect(LAYER_1_X, LAYER_1_Y, LAYER_1_X + LAYER_1_WIDTH - 1, LAYER_1_Y + LAYER_1_HEIGHT - 1);
+	//s_surface->show_overlapped_rect(overlapped_rect, Z_ORDER_LEVEL_0);
+
+	//animation
+	for (int offset = 0; offset < LAYER_1_WIDTH / 2; offset++)
+	{
+		c_rect overlapped_rect_top(LAYER_1_X, LAYER_1_Y + offset, LAYER_1_X + LAYER_1_WIDTH - 1, LAYER_1_Y + offset + 1);
+		s_surface->show_overlapped_rect(overlapped_rect_top, Z_ORDER_LEVEL_0);
+
+		c_rect overlapped_rect_bottom(LAYER_1_X, LAYER_1_Y + LAYER_1_HEIGHT - offset - 2, LAYER_1_X + LAYER_1_WIDTH - 1, LAYER_1_Y + LAYER_1_HEIGHT - offset - 1);
+		s_surface->show_overlapped_rect(overlapped_rect_bottom, Z_ORDER_LEVEL_0);
+
+		thread_sleep(5);
+	}
+}
+
+void create_ui(void* phy_fb, int screen_width, int screen_height, int color_bytes, struct EXTERNAL_GFX_OP* gfx_op) {
+	if (phy_fb)
+	{
+		static c_surface surface(UI_WIDTH, UI_HEIGHT, color_bytes, Z_ORDER_LEVEL_1, c_rect(LAYER_1_X, LAYER_1_Y, LAYER_1_X + LAYER_1_WIDTH - 1, LAYER_1_Y + LAYER_1_HEIGHT - 1));
+		static c_display display(phy_fb, screen_width, screen_height, &surface);
+		s_surface = &surface;
+		s_display = &display;
+	}
+	else
+	{//for MCU without framebuffer
+		static c_surface_no_fb surface_no_fb(UI_WIDTH, UI_HEIGHT, color_bytes, gfx_op, Z_ORDER_LEVEL_1, c_rect(LAYER_1_X, LAYER_1_Y, LAYER_1_X + LAYER_1_WIDTH - 1, LAYER_1_Y + LAYER_1_HEIGHT - 1));
+		static c_display display(phy_fb, screen_width, screen_height, &surface_no_fb);
+		s_surface = &surface_no_fb;
+		s_display = &display;
+	}
+
+	draw_on_layer_0();
+	while(1) {
+		draw_on_layer_1();
+		thread_sleep(2000);
+		clear_layer_1();
+		thread_sleep(2000);
+	}
+}
+
+//////////////////////// interface for all platform ////////////////////////
+extern "C" void startHelloLayers(void* phy_fb, int width, int height, int color_bytes, struct EXTERNAL_GFX_OP* gfx_op) {
+	create_ui(phy_fb, width, height, color_bytes, gfx_op);
+}
+
+void* getUiOfHelloLayers(int* width, int* height, bool force_update)
+{
+	if (s_display)
+	{
+		return s_display->get_updated_fb(width, height, force_update);
+	}
+	return NULL;
+}

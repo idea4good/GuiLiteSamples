3d90018ddc156db8dcd440d3f3c4b9fdc193dfee
idea4good <idea4good@outlook.com>
2020-10-15 12:41:46 +0800

    fix hostMonitor/buildDocker issue

--- HostMonitor/BuildDocker/build.sh
+++ HostMonitor/BuildDocker/build.sh
@@ -11,8 +11,8 @@ test_and_build()
         rm CMakeCache.txt
         cmake $4 . && make
         cd BuildDocker
+        mv ../BuildLinux/HostMonitor $1/HostMonitor
     fi
-    cp ../BuildLinux/HostMonitor $1/HostMonitor
 
     docker rmi -f $2
     docker build --build-arg input=$1 --build-arg base=$3 -t $2 .
@@ -23,6 +23,7 @@ test_and_build x64 $image_x64 ubuntu
 test_and_build arm $image_arm arm32v7/ubuntu "-D CMAKE_C_COMPILER='/usr/bin/arm-linux-gnueabihf-gcc' -D CMAKE_CXX_COMPILER='/usr/bin/arm-linux-gnueabihf-g++'"
 
 # release
+docker login
 docker tag $image_x64 idea4good/$image_x64
 docker push idea4good/$image_x64
 
@@ -32,7 +33,10 @@ docker push idea4good/$image_arm
 docker manifest create idea4good/$image_all idea4good/$image_x64 idea4good/$image_arm
 docker manifest annotate idea4good/$image_all idea4good/$image_x64 --arch amd64
 docker manifest annotate idea4good/$image_all idea4good/$image_arm --arch arm
-docker manifest push idea4good/$image_all
+docker manifest push idea4good/$image_all --purge
 
-# testing
-# docker run -it $image_x64
+# check & testing
+docker image inspect $image_x64
+docker run -it $image_x64
+docker image inspect $image_arm
+docker run -it $image_arm
